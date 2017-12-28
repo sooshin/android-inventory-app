37f2e8d7659069143e2bbd6963f24c95f958bdb8
Soojeong <soojeongshin2@gmail.com>
2017-12-28 19:57:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.inventory;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+    }
+}
