fde6aebf15115c0bca15ecafe456acb34a10e1d8
Soojeong <soojeongshin2@gmail.com>
2017-12-28 23:50:45 +0900

    Create Database with SQLiteOpenHelper

--- app/src/main/java/com/example/android/inventory/MainActivity.java
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.android.inventory;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.inventory.data.ProductDbHelper;
+import com.example.android.inventory.data.ProductContract.ProductEntry;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +25,34 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
             }
         });
+
+        displayDatabaseInfo();
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen TextView about the state of
+     * the products database.
+     */
+    private void displayDatabaseInfo() {
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        ProductDbHelper mDbHelper = new ProductDbHelper(this);
+
+        // Create and/or open a database to read from it
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        // Perform this raw SQL query "SELECT * FROM products"
+        // to get a Cursor that contains all rows from the products table.
+        Cursor cursor = db.rawQuery("SELECT * FROM " + ProductEntry.TABLE_NAME, null);
+        try {
+            // Display the number of rows in the Cursor (which reflects the number of rows in the
+            // products table in the database).
+            TextView displayView = findViewById(R.id.text_view_product);
+            displayView.setText("Number of rows in products database table: " + cursor.getCount());
+        } finally {
+            // Always close the cursor when you're done reading from it. This releases all its
+            // resources and makes it invalid.
+            cursor.close();
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/inventory/data/ProductDbHelper.java
@@ -0,0 +1,57 @@
+package com.example.android.inventory.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.android.inventory.data.ProductContract.ProductEntry;
+
+/**
+ * Database helper for Inventory app. Manages database creation and version management.
+ */
+public class ProductDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = ProductDbHelper.class.getSimpleName();
+
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "inventory.db";
+
+    /** Database version. If you change the database schema, you must increment the database version. */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link ProductDbHelper}.
+     * @param context of the app
+     */
+    public ProductDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * This is called when the database is created for the first time.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a String that contains the SQL statement to create the products table
+        String SQL_CREATE_PRODUCTS_TABLE = "CREATE TABLE " + ProductEntry.TABLE_NAME + " ("
+                + ProductEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_PRICE + " INTEGER NOT NULL, "
+                + ProductEntry.COLUMN_QUANTITY + " INTEGER NOT NULL, "
+                + ProductEntry.COLUMN_PRODUCT_IMAGE + " TEXT, "
+                + ProductEntry.COLUMN_SUPPLIER_NAME + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_SUPPLIER_EMAIL + " TEXT NOT NULL, "
+                + ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER + " TEXT NOT NULL);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_PRODUCTS_TABLE);
+    }
+
+    /**
+     * This is called when the database needs to be upgraded.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // The database is still at version 1, so there's nothing to do be done here.
+    }
+}
