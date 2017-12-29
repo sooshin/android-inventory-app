e1e5247ecdf8cfef79890ac6c6359bfa53e8badb
Soojeong <soojeongshin2@gmail.com>
2017-12-29 19:23:43 +0900

    Setup the Menu item in the EditorActivity

--- app/src/main/java/com/example/android/inventory/EditorActivity.java
+++ app/src/main/java/com/example/android/inventory/EditorActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.inventory;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 /**
@@ -40,5 +43,34 @@ public class EditorActivity extends AppCompatActivity {
         mSupplierNameEditText = findViewById(R.id.edit_supplier_name);
         mSupplierEmailEditText = findViewById(R.id.edit_supplier_email);
         mSupplierPhoneEditText = findViewById(R.id.edit_supplier_phone);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (MainActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
