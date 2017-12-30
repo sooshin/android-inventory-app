c7b87d789382c2d6f7344a7933e882f982cfed91
Soojeong <soojeongshin2@gmail.com>
2017-12-30 17:17:01 +0900

    Remove LOG TAG

--- app/src/main/java/com/example/android/inventory/data/ProductDbHelper.java
+++ app/src/main/java/com/example/android/inventory/data/ProductDbHelper.java
@@ -11,8 +11,6 @@ import com.example.android.inventory.data.ProductContract.ProductEntry;
  */
 public class ProductDbHelper extends SQLiteOpenHelper {
 
-    public static final String LOG_TAG = ProductDbHelper.class.getSimpleName();
-
     /** Name of the database file */
     private static final String DATABASE_NAME = "inventory.db";
 
