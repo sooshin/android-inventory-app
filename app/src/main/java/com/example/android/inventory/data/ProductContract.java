5e4ab8654881f0c9091681ddf70b8ec7a71151d3
Soojeong <soojeongshin2@gmail.com>
2017-12-31 21:58:57 +0900

    Add URIs to the Contract

--- app/src/main/java/com/example/android/inventory/data/ProductContract.java
+++ app/src/main/java/com/example/android/inventory/data/ProductContract.java
@@ -1,5 +1,6 @@
 package com.example.android.inventory.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -14,10 +15,36 @@ public final class ProductContract {
     }
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.inventory";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.inventory/products/ is a valid path for
+     * looking at product data. content://com.example.android.inventory/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PRODUCT = "products";
+
+
+    /**
      *  Inner class that defines constant values for the products database table.
      *  Each entry in the table represents a single product.
      */
     public static final class ProductEntry implements BaseColumns {
+
+        /** The content URI to access the product data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCT);
 
         /** Name of database table for products */
         public static final String TABLE_NAME = "products";
