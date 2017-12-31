5b3b5ba11967b012aec5af16cdb9ea27afb5ec46
Soojeong <soojeongshin2@gmail.com>
2017-12-31 22:16:05 +0900

    Setup URI Matcher in ProductProvider

--- app/src/main/java/com/example/android/inventory/data/ProductProvider.java
+++ app/src/main/java/com/example/android/inventory/data/ProductProvider.java
@@ -2,6 +2,7 @@ package com.example.android.inventory.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -9,6 +10,29 @@ import android.net.Uri;
  * {@link ContentProvider} for Inventory app.
  */
 public class ProductProvider extends ContentProvider{
+
+    /** URI matcher code for the content URI for the products table */
+    private static final int PRODUCTS = 100;
+
+    /** URI matcher code for the content URI for a single product in the products table */
+    private static final int PRODUCT_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY, ProductContract.PATH_PRODUCT, PRODUCTS);
+        sUriMatcher.addURI(ProductContract.CONTENT_AUTHORITY,
+                ProductContract.PATH_PRODUCT + "/#", PRODUCT_ID);
+    }
 
     /** Tag for the log messages */
     public static final String LOG_TAG = ProductProvider.class.getSimpleName();
