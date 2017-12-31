9d17af15a3bc12d1f182ac9e9d98de88cb9d8064
Soojeong <soojeongshin2@gmail.com>
2017-12-31 22:46:59 +0900

    Implement ProductProvider query() method

--- app/src/main/java/com/example/android/inventory/data/ProductProvider.java
+++ app/src/main/java/com/example/android/inventory/data/ProductProvider.java
@@ -1,10 +1,14 @@
 package com.example.android.inventory.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+
+import com.example.android.inventory.data.ProductContract.ProductEntry;
 
 /**
  * {@link ContentProvider} for Inventory app.
@@ -56,7 +60,44 @@ public class ProductProvider extends ContentProvider{
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                // For the PRODUCTS code, query the products table directly with the given
+                // projection, selection, selection arguments, and sort order. The cursor
+                // could contain multiple rows of the products table.
+                // Perform database query on products table
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            case PRODUCT_ID:
+                // For the PRODUCT_ID code, extract out the ID from the URI.
+                // For an example URI such as "content://com.example.android.inventory/products/3",
+                // the selection will be "_id=?" and the selection argument will be a
+                // String array containing the actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in the selection
+                // arguments that will fill in the "?". Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = ProductEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                // This will perform a query on the products table where the _id equals 3 to
+                // return a Cursor containing that row of the table.
+                cursor = database.query(ProductEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     /**
