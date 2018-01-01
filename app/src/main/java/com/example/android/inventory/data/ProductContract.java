842804fc37deb4a3277a45b9c4ac90d9af55bfd8
Soojeong <soojeongshin2@gmail.com>
2018-01-01 21:35:10 +0900

    Declare MIME Type constants in ProductContract

--- app/src/main/java/com/example/android/inventory/data/ProductContract.java
+++ app/src/main/java/com/example/android/inventory/data/ProductContract.java
@@ -1,5 +1,6 @@
 package com.example.android.inventory.data;
 
+import android.content.ContentResolver;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
@@ -45,6 +46,18 @@ public final class ProductContract {
 
         /** The content URI to access the product data in the provider */
         public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCT);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCT;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single product.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PRODUCT;
 
         /** Name of database table for products */
         public static final String TABLE_NAME = "products";
