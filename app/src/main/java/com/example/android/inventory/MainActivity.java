d0106a84a02e3d85f6c1fd424531dba3c4379053
Soojeong <soojeongshin2@gmail.com>
2018-01-02 15:17:21 +0900

    Display List of Products with ListView and CursorAdapter

--- app/src/main/java/com/example/android/inventory/MainActivity.java
+++ app/src/main/java/com/example/android/inventory/MainActivity.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.example.android.inventory.data.ProductContract.ProductEntry;
 
@@ -51,72 +51,12 @@ public class MainActivity extends AppCompatActivity {
     private void displayDatabaseInfo() {
         Cursor cursor = queryProduct();
 
-        TextView displayView = findViewById(R.id.text_view_product);
-
-        try {
-            // Create a header in the Text View that looks like this:
-            // The products table contains <number of rows in Cursor> products.
-            // _id - product_name -author - publisher - isbn - price - quantity - supplier_name - supplier_email - supplier_phone_number
-
-            displayView.setText("The products table contains " + cursor.getCount() + " products.\n\n");
-            displayView.append(ProductEntry._ID + " - " +
-                    ProductEntry.COLUMN_PRODUCT_NAME + " - " +
-                    ProductEntry.COLUMN_PRODUCT_AUTHOR + " - " +
-                    ProductEntry.COLUMN_PRODUCT_PUBLISHER + " - " +
-                    ProductEntry.COLUMN_PRODUCT_ISBN + " - " +
-                    ProductEntry.COLUMN_PRICE + " - " +
-                    ProductEntry.COLUMN_QUANTITY + " - " +
-                    ProductEntry.COLUMN_SUPPLIER_NAME + " - " +
-                    ProductEntry.COLUMN_SUPPLIER_EMAIL + " - " +
-                    ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER + "\n");
-
-            // Figure out the index of each column
-            int idColumnIndex = cursor.getColumnIndex(ProductEntry._ID);
-            int productNameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
-            int productAuthorColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_AUTHOR);
-            int productPublisherColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PUBLISHER);
-            int productIsbnColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_ISBN);
-            int priceColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRICE);
-            int quantityColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_QUANTITY);
-            int supplierNameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_SUPPLIER_NAME);
-            int supplierEmailColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_SUPPLIER_EMAIL);
-            int supplierPhoneColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_SUPPLIER_PHONE_NUMBER);
-
-            // Check if the Cursor has items before calling the method moveToNext().
-            if (cursor.moveToFirst()) {
-                // Iterate through all the returned rows in the cursor
-                do {
-                    // Use that index to extract the String or int value of the word
-                    // at the current row the cursor is on.
-                    int currentID = cursor.getInt(idColumnIndex);
-                    String currentProductName = cursor.getString(productNameColumnIndex);
-                    String currentProductAuthor = cursor.getString(productAuthorColumnIndex);
-                    String currentProductPublisher = cursor.getString(productPublisherColumnIndex);
-                    String currentProductIsbn = cursor.getString(productIsbnColumnIndex);
-                    int currentPrice = cursor.getInt(priceColumnIndex);
-                    int currentQuantity = cursor.getInt(quantityColumnIndex);
-                    String currentSupplierName = cursor.getString(supplierNameColumnIndex);
-                    String currentSupplierEmail = cursor.getString(supplierEmailColumnIndex);
-                    String currentSupplierPhone = cursor.getString(supplierPhoneColumnIndex);
-
-                    // Display the values from each column of the current row in the cursor in the TextView
-                    displayView.append(("\n" + currentID + " - " +
-                            currentProductName + " - " +
-                            currentProductAuthor + " - " +
-                            currentProductPublisher + " - " +
-                            currentProductIsbn + " - " +
-                            currentPrice + " - " +
-                            currentQuantity + " - " +
-                            currentSupplierName + " - " +
-                            currentSupplierEmail + " - " +
-                            currentSupplierPhone));
-                } while (cursor.moveToNext());
-            }
-        } finally {
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
-            cursor.close();
-        }
+        // Find the ListView which will be populated with the product data
+        ListView productListView = findViewById(R.id.list);
+        // Setup a CursorAdapter to create a list item for each row of product data in the Cursor.
+        ProductCursorAdapter cursorAdapter = new ProductCursorAdapter(this, cursor);
+        // Attach the cursor adapter to the ListView
+        productListView.setAdapter(cursorAdapter);
     }
 
     /**
---
+++ app/src/main/java/com/example/android/inventory/ProductCursorAdapter.java
@@ -0,0 +1,76 @@
+package com.example.android.inventory;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.inventory.data.ProductContract.ProductEntry;
+
+/**
+ * {@link ProductCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of product data as its data source. This adapter knows
+ * how to create list items for each row of product data in the {@link Cursor}.
+ */
+public class ProductCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link ProductCursorAdapter}.
+     *
+     * @param context The context
+     * @param c        The cursor from which to get the data.
+     */
+    public ProductCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor The cursor from which to get the data. The cursor is already moved to the
+     *                 correct position.
+     * @param parent The parent to which the view is attached
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the product data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the product name for the current product can be set on the
+     * product name TextView in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor The cursor from which to get the data. The cursor is already moved to the
+     *                 correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find individual views that we want to modify in the list item layout
+        TextView productNameTextView = view.findViewById(R.id.product_name);
+        TextView authorTextView = view.findViewById(R.id.product_author);
+        TextView quantityTextView = view.findViewById(R.id.product_quantity);
+
+        // Find the columns of product attributes that we're interested in
+        int productNameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
+        int authorColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_AUTHOR);
+        int quantityColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_QUANTITY);
+
+        // Read the product attributes from the Cursor for the current product
+        String productName = cursor.getString(productNameColumnIndex);
+        String author = cursor.getString(authorColumnIndex);
+        int quantity = cursor.getInt(quantityColumnIndex);
+
+        // Update the TextViews with the attributes for the current product
+        productNameTextView.setText(productName);
+        authorTextView.setText(author);
+        quantityTextView.setText(String.valueOf(quantity));
+    }
+}
