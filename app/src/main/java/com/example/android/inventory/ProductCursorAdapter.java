aabe91c0026cfa4cbc9c0b16b2ca8c6ba94be69d
Soojeong <soojeongshin2@gmail.com>
2018-01-09 21:04:49 +0900

    Set image in the list view

--- app/src/main/java/com/example/android/inventory/ProductCursorAdapter.java
+++ app/src/main/java/com/example/android/inventory/ProductCursorAdapter.java
@@ -4,7 +4,11 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +20,10 @@ import android.widget.Toast;
 
 import com.example.android.inventory.data.ProductContract.ProductEntry;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * {@link ProductCursorAdapter} is an adapter for a list or grid view
  * that uses a {@link Cursor} of product data as its data source. This adapter knows
@@ -24,6 +32,8 @@ import com.example.android.inventory.data.ProductContract.ProductEntry;
 public class ProductCursorAdapter extends CursorAdapter {
 
     private Context mContext;
+
+    private static final String LOG_TAG = ProductCursorAdapter.class.getSimpleName();
     /**
      * Constructs a new {@link ProductCursorAdapter}.
      *
@@ -85,6 +95,11 @@ public class ProductCursorAdapter extends CursorAdapter {
         String imageString = cursor.getString(imageColumnIndex);
         final long id = cursor.getLong(idColumnIndex);
 
+        // Set image in the list view
+        if (!TextUtils.isEmpty(imageString)) {
+            imageView.setImageBitmap(getBitmapFromUri(Uri.parse(imageString), view));
+        }
+
         // If the quantity is more than 0, set the text of a sale button to display 'sell'.
         // Otherwise, set the text of a sale button to display 'sold out'.
         if (quantity > 0) {
@@ -136,5 +151,61 @@ public class ProductCursorAdapter extends CursorAdapter {
         authorTextView.setText(author);
         priceTextView.setText(String.valueOf(price));
         quantityTextView.setText(String.valueOf(quantity));
+    }
+
+    /**
+     * Returns a Bitmap object from the URI which is the location of the image.
+     */
+    public Bitmap getBitmapFromUri(Uri uri , View view) {
+        ImageView mImageView = view.findViewById(R.id.product_image);
+
+        // Check the Uri is null or empty
+        if (uri == null || uri.toString().isEmpty()) {
+            return null;
+        }
+
+        // Get the dimensions of the View
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        InputStream inputStream = null;
+        try {
+            inputStream = mContext.getContentResolver().openInputStream(uri);
+
+            // Get the dimensions of the bitmap
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(inputStream, null, bmOptions);
+            inputStream.close();
+
+            int photoW = bmOptions.outWidth;
+            int photoH = bmOptions.outHeight;
+
+            // Determine how much to scale down the image
+            int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inSampleSize = scaleFactor;
+            bmOptions.inPurgeable = true;
+
+            inputStream = mContext.getContentResolver().openInputStream(uri);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream, null, bmOptions);
+            inputStream.close();
+            return bitmap;
+
+        } catch (FileNotFoundException fne) {
+            Log.e(LOG_TAG, "Failed to open the image file.", fne);
+            return null;
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Failed to load image.", e);
+            return null;
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Problem with loading a file.");
+            }
+        }
     }
 }
